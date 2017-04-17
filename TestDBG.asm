e8b5c006ddec0f800a8d9b0e4d7e87400ec2c3d1
Leila Aparecida da Silva <leilaapsilva26@gmail.com>
2017-04-17 11:41:03 -0300

    Add files via upload

--- TestDBG.asm
+++ TestDBG.asm
@@ -4,15 +4,18 @@ Include ..\Irvine32.inc
 
 .data 
 
-val1 	DWORD  0
+
+val1 	DWORD  0	;ex 1 
 val2	DWORD  0
 val3	DWORD  0 
 val4	DWORD  0
-val5 	DWORD  0 
-val6 	DWORD  0 
+
+val5 	DWORD  0 	; ex 2 
+			
+val6 	DWORD  0 	; ex 3 
 val7 	DWORD  0 
 
-byte1 	BYTE	'A'
+byte1 	BYTE	'A' ; ex 4
 byte2	BYTE   	 0
 byte3	BYTE   	 255
 byte4	SBYTE  	-128
@@ -28,20 +31,38 @@ dword2	SDWORD	-2147483648
 
 quad1	QWORD	0123456789ABCDEFh
 
+count 	NAME  	500d ; ex 5 - a 
+
+rows 	EQU	10 	; ex 5 - b 
+cols	EQU 15 
+msize 	EQU rows * cols 
+
+
+
 
 .code
 main PROC
   int 3 
   
+	; ex 1 
 	mov val1, 100
 	mov val2, 100b
 	mov val3, 64h
 	mov val4, 01100100b
 	
+	; ex 2 
 	mov val5, 100 + 100d + 64h + 01100100b
 	
+	; ex 3 
 	mov val6, 'a'
 	mov val7, "abcd"
+	
+	; ex 5 - a 
+	mov eax, count 
+	mov cout, 100d 
+	add eax, count 
+	
+	;;;;; 
 	
 	mov eax,5 ; move 5 to the eax register
 	add eax,6 ; add 6 to the eax register
